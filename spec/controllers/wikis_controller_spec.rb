731075373e14a06735bb76c628de571891e4b556
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-05-04 00:34:32 +0000

    updated specs

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe WikisController, type: :controller do
+  
+    let(:my_user) { create(:user) }
+    let(:my_wiki) { create(:wiki, user: my_user) }
 
   describe "GET #index" do
     it "returns http success" do
@@ -9,25 +12,37 @@ RSpec.describe WikisController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
+   describe "GET show" do
+     it "returns http success" do
+       get :show, {id: my_wiki.id}
+       expect(response).to have_http_status(:success)
+     end
+     it "renders the #show view" do
+       get :show, {id: my_wiki.id}
+       expect(response).to render_template :show
+     end
+ 
+     it "assigns my_wiki to @wiki" do
+       get :show, {id: my_wiki.id}
+       expect(assigns(:wiki)).to eq(my_wiki)
+     end
+   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe "GET new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
+      it "renders the #new view" do
+        get :new
+        expect(response).to render_template :new
+      end
+
+      it "initializes @wiki" do
+        get :new
+        expect(assigns(:wiki)).not_to be_nil
+      end
     end
-  end
 
 end
