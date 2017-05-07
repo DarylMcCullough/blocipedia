d41c0fb09a539903455020e6a9bf2a10aba9cbe5
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-05-07 20:38:26 +0000

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,46 @@
  require 'random_data'
+ # Create Users
+ 5.times do
+   user = User.new(
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+   user.confirm
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+ 
+ # create wikis
+    10.times do
+     Wiki.create!(
+     user: users.sample,
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+     )
+    end
 
+  # Create an admin user
+ admin = User.new(
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.confirm
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a premium user
+ premium = User.new(
+     email: 'premium@example.com',
+     password: 'helloworld',
+     role: 'premium')
+ premium.confirm
+ premium.skip_confirmation!
+ premium.save!
+
+
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Wiki.count} wikis created"
+ 
