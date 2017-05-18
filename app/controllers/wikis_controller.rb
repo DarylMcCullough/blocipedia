f55da417d0b66162bd1c2944c348574f905c6cf4
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-05-18 17:41:09 +0000

    refactor to use Pundit policies for scope and authorization

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,101 +1,62 @@
 class WikisController < ApplicationController
   def index
-      if ! current_user.present? || current_user.standard?
-          @wikis = Wiki.where(private: false)
-          return
-      end
-      @wikis = Wiki.all
+      @wikis = policy_scope(Wiki)
   end
 
-  def show
-    @wiki = Wiki.find(params[:id])
-  end
-
-  def new
-    @wiki = Wiki.new
-    begin
-        authorize @wiki
-    rescue
-        flash[:alert] = "You must be logged in to create a Wiki."
-        redirect_to @wiki
-        return
+    def show
+        @wiki = Wiki.find(params[:id])
     end
-  end
-  
-  def create
-      
-    @wiki = Wiki.new(wiki_params)
-    @wiki.user = current_user
 
-    begin
-         authorize @wiki
-     rescue => error
-        if ! current_user.present?
-            alert = "You must be logged in to create a wiki"
-        elsif current_user.standard? && @wiki.private
-            alert = "You are not authorized to create private wikis"
+    def new
+        @wiki = Wiki.new
+        authorize @wiki
+    end
+  
+    def create
+        @wiki = Wiki.new(wiki_params)
+        @wiki.user = current_user
+        if @wiki.save!
+            redirect_to @wiki, notice: "Wiki was saved successfully."
         else
-            alert = "You are not authorized to do that, for some reason"
+            flash.now[:alert] = "Error creating wiki. Please try again."
+            render :new
         end
-        flash[:alert] = alert
-       redirect_to action: :index
-       return
-     end
-     
-     if @wiki.save!
-       redirect_to @wiki, notice: "Wiki was saved successfully."
-     else
-       flash.now[:alert] = "Error creating wiki. Please try again."
-       render :new
-     end
-   end
+    end
 
     def edit
         @wiki = Wiki.find(params[:id])
-        begin
-            authorize @wiki
-        rescue
-            flash[:alert] = "You must be logged in to edit this Wiki."
-            redirect_to action: :index
-            return
+        authorize @wiki
+    end
+  
+    def update
+        @wiki = Wiki.find(params[:id])
+        authorize @wiki
+        @wiki.assign_attributes(wiki_params)
+ 
+        if @wiki.save
+            flash[:notice] = "Wiki was updated."
+            redirect_to @wiki
+        else
+            flash.now[:alert] = "Error saving wiki. Please try again."
+            render :edit
         end
-  end
+    end
   
-  def update
-     @wiki = Wiki.find(params[:id])
-     begin
-         authorize @wiki
-     rescue
-        flash[:alert] = "You must be logged in to edit this Wiki."
-       redirect_to @wiki
-       return
-     end
-     @wiki.assign_attributes(wiki_params)
+    def destroy
+        @wiki = Wiki.find(params[:id])
  
-     if @wiki.save
-        flash[:notice] = "Wiki was updated."
-       redirect_to @wiki
-     else
-       flash.now[:alert] = "Error saving wiki. Please try again."
-       render :edit
-     end
-  end
+        if @wiki.destroy
+            flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+            redirect_to action: :index
+        else
+            flash.now[:alert] = "There was an error deleting the wiki."
+            render :show
+        end
+    end
   
-  def destroy
-     @wiki = Wiki.find(params[:id])
+    private
  
-     if @wiki.destroy
-       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
-       redirect_to action: :index
-     else
-       flash.now[:alert] = "There was an error deleting the wiki."
-       render :show
-     end
-  end
-  
-  private
- 
-   def wiki_params
-     params.require(:wiki).permit(:title, :private, :body)
-   end
+    def wiki_params
+        params.require(:wiki).permit(:title, :private, :body)
+    end
 end
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -2,30 +2,49 @@ class WikiPolicy < ApplicationPolicy
   
   attr_reader :user, :wiki
   
+  class Scope < Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user  = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.present? && (user.admin? || user.premium?)
+        scope.all
+      else
+        scope.where(private: false)
+      end
+    end
+  end
+  
   def initialize(user, wiki)
     @user = user
     @wiki = wiki
   end
   
-  def update?
+  def update? # user must be logged in to update a wiki
     user.present?
   end
   
-  def edit?
+  def edit? # user must be logged in to edit a wiki
     user.present?
   end
   
-  def new?
+  def new? # user must be logged in to create a wiki
     user.present?
   end
   
-  def create? # user must be logged in to create a wiki
-    if ! user.present?
+  def destroy?
+    user.present? && user.admin? # only an admin can destroy a wiki
+  end
+  
+  def create?
+    if ! user.present?  # user must be logged in to create a wiki
       return false
     end
-    if wiki.private
-      return user.admin? || user.premium?
-    end
-    return true
+    # only admins and premium users can create private wikis
+    (! wiki.private) || user.admin? || user.premium?
   end
 end
