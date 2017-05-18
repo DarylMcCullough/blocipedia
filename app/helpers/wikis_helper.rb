bc80f31c1f77d38834ca1c7424d2cba010a6a882
Daryl McCullough <stevendaryl3016@yahoo.com>
2017-05-18 20:26:35 +0000

    remove (now redundant) authorization checks in WikisHelper

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,9 +1,2 @@
 module WikisHelper
-    def user_is_authorized_for_wikis?
-        return current_user.present?
-   end
-   
-   def user_is_authorized_for_wiki?(wiki)
-        return current_user.present?
-   end
 end
